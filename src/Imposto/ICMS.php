7c361b036dd1064a16a2d712781e3716e6a34861
kassielbatista <kassielbruno@hotmail.com>
2017-04-02 22:37:54 -0300

    ICMS retornando a soma dos dois impostos.

--- src/Imposto/ICMS.php
+++ src/Imposto/ICMS.php
@@ -1,14 +1,25 @@
 <?php
 
-
 namespace Study\Imposto;
 
 
 class ICMS implements IImposto
 {
+    private $ipi;
+
+    /**
+     * ICMS constructor.
+     * @param IPI $ipi
+     */
+    public function __construct(IPI $ipi = null)
+    {
+        $this->ipi = $ipi;
+    }
 
     public function calcula(float $valor): float
     {
-        return ($valor / 100) * 3;
+        $ipi = empty($this->ipi) ? 0 : $this->ipi->calcula($valor);
+
+        return $valor  * 0.03 + $ipi;
     }
 }
